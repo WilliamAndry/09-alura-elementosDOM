b55a3c3095c8917ff990cd56fdfb4761870c4f43
William Andry <williamandrysm98@gmail.com>
2024-07-01 13:46:28 -0600

    Modificando imagenes y textos por eventos de click

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const html = document.querySelector('html')
+const hmtl = document.querySelector('html')
 const botonCorto = document.querySelector('.app__card-button--corto')
 const botonEnfoque = document.querySelector('.app__card-button--enfoque')
 const botonLargo = document.querySelector('.app__card-button--largo')
 const banner = document.querySelector('.app__image')
+const titulo= document.querySelector('.app__title')
 
-botonCorto.addEventListener('click',()=>{
-    html.setAttribute('data-contexto','descanso-corto')
-    banner.setAttribute('src','./imagenes/descanso-corto.png')
+ botonCorto.addEventListener('click',()=>{
+    cambiarContexto('descanso-corto')
 })
 
 botonEnfoque.addEventListener('click',()=>{
-
-    html.setAttribute('data-contexto','enfoque')
-    banner.setAttribute('src','./imagenes/enfoque.png')
+    cambiarContexto('enfoque')
 })
 
 botonLargo.addEventListener('click',()=>{
-    html.setAttribute('data-contexto','descanso-largo')
-    banner.setAttribute('src','./imagenes/descanso-largo.png')
+    cambiarContexto('descanso-largo')
 })
 
+ function cambiarContexto(contexto){
+    hmtl.setAttribute('data-contexto',contexto)
+    banner.setAttribute('src',`./imagenes/${contexto}.png`)
+
+    switch(contexto){
+        case 'enfoque':
+            titulo.innerHTML=
+            `Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>`
+        break;
+
+        case 'descanso-corto':
+            titulo.innerHTML=
+            `¿Que tal tomar un respiro?
+                <strong class="app__title-strong">¡Haz una pausa corta!.</strong>`
+        break;
+
+        case 'descanso-largo':
+            titulo.innerHTML=
+            `Hora de volver a la superficie
+                <strong class="app__title-strong">Haz una paua larga.</strong>`
+        break;
+    }
+} 
